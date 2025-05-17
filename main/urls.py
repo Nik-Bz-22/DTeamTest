7d7eae92a8a31edb3c4e596ecedfc8371ddbac85
Nik-BZ-22 <bakhamyt@gmail.com>
2025-05-17 19:00:43 +0300

    Task 5: Template Context Processors

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CVListView, CVDetailView, download_cv_pdf, CVViewSet
+from .views import CVListView, CVDetailView, download_cv_pdf, CVViewSet, settings_view
 from rest_framework.routers import DefaultRouter
 
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("", CVListView.as_view(), name="cv_list"),
     path("cv/<int:pk>/", CVDetailView.as_view(), name="cv_detail"),
     path("cv/<int:pk>/download/", download_cv_pdf, name="download_cv_pdf"),
+    path("settings/", settings_view, name="settings"),
 ] + router.urls
